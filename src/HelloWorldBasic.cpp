f6b91c1b3466084c850df3a30dd9c1da0301d440
Dr. YFish <261328128+dryfish09@users.noreply.github.com>
2026-08-29 10:28:14 +0700

    Create HelloWorldBasic.cpp

---
+++ src/HelloWorldBasic.cpp
@@ -0,0 +1,11 @@
+// filename: HelloWorldBasic.cpp
+// license: Unlicense
+// purpose: educational only, not production ready.
+#include <iostream> // required, to print out the screen.
+using namespace std; // shorter way to call function from imported library (like this, iostream)
+// if you don't have this, you should write std::cout, std::cin.
+int main() {
+    // main is entry point, required for any program.
+    cout << "Hello, World!" << endl; // endl create new line
+    return 0; // exit with code 0 (success, no error)
+}
