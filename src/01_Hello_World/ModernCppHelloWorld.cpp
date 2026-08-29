c503f3f7426f3334651a404f3241e212f02ec013
Dr. YFish <261328128+dryfish09@users.noreply.github.com>
2026-08-29 16:00:36 +0700

    Create ModernCppHelloWorld.cpp

---
+++ src/01_Hello_World/ModernCppHelloWorld.cpp
@@ -0,0 +1,13 @@
+/*
+filename: ModernCppHelloWorld.cpp
+license: Unlicense
+purpose: educational only, not production ready.
+note: this code requires modern c++ version. (like c++ 23+, or 26+, i don't know)
+*/
+#include <print>
+using namespace std;
+int main() {
+    println("Hello, World"); // println will add newline, print doesn't create newline. If you come from kotlin, you will know it.
+    return 0;
+}
+	
