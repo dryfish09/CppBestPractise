af8ce6a85065a8f4250306259832a505a949845b
Dr. YFish <261328128+dryfish09@users.noreply.github.com>
2026-08-30 12:08:42 +0700

    fix: ok info

--- src/01_Hello_World/ModernCppHelloWorld.cpp
+++ src/01_Hello_World/ModernCppHelloWorld.cpp
@@ -2,12 +2,12 @@
 filename: ModernCppHelloWorld.cpp
 license: Unlicense
 purpose: educational only, not production ready.
-note: this code requires modern c++ version. (like c++ 23+, or 26+, i don't know)
+note: this code requires modern c++ version. (C++ 23+)
 */
 #include <print>
 using namespace std;
 int main() {
-    println("Hello, World"); // println will add newline, print doesn't create newline. If you come from kotlin, you will know it.
+    println("Hello, World!"); // println will add newline, print doesn't create newline. If you come from kotlin, you will know it.
     return 0;
 }
 	
