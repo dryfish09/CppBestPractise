6596ca6d6337965a5aff982412c6391dea6c5aa4
Dr. YFish <261328128+dryfish09@users.noreply.github.com>
2026-08-30 14:38:40 +0700

    Add more example

--- src/03_Conditions/IfAndElse.cpp
+++ src/03_Conditions/IfAndElse.cpp
@@ -14,7 +14,35 @@ int main() {
         std::cout<< "you're older than 13 years old" <<std::endl;
     } else {
         // else - if all check failed, do this
-        std::cout << "you younger than me" << std::endl;
+        std::cout << "you younger than 13 years old." << std::endl;
+    }
+    int score = 40;
+    bool isAlive = true; //  new variable: bool (boolean). Only true and false.
+    if (score > 50 && isAlive) {
+        std::cout << "You win!" << std::endl; // only print out when score HIGHER or EQUAL 50 AND isAlive = true
+    }
+    else if (score <= 50 && isAlive) {
+        // only print when score < or = 50 and isAlive = true
+        std::cout << "You still alive, but your score doesn't enough" << std::endl;
+    }
+    else if (!isAlive) {
+        // only print when isAlive  = false
+        std::cout << "you died" << std::endl;
+    } else {
+        std::cout << "unknown condition" << std::endl;
     }
     return 0;
 }
+/*
+Comparison Syntax:
+conditions (cond)
+- var1 == var2 (or values): equal
+- var1 != var2 (or values): not equal
+- cond1 && cond2: and
+- cond1 || cond2: or
+
+Meaning:
+- var: variables
+- cond: condition
+REMEMBER: use '==' for 'if' or 'else if' condition, not '='
+*/
