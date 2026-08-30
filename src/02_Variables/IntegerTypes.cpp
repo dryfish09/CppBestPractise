23927c435f7e297728a32b617dade8f96673e7fc
Dr. YFish <261328128+dryfish09@users.noreply.github.com>
2026-08-30 10:34:44 +0700

    Update IntegerTypes.cpp

--- src/02_Variables/IntegerTypes.cpp
+++ src/02_Variables/IntegerTypes.cpp
@@ -4,15 +4,15 @@ License: The Unlicense
 */
 #include <iostream>
 int main() {
-    // int - archive normal number, for normal use.
+    // int - store normal number, for normal use.
     int favouriteNumber = 36;
     std::cout << "my favourite numberis: " << favouriteNumber << std::endl;
-    // short - only archive small number (like age, classroom on your school, etc)
+    // short - only store small number (like age, classroom on your school, etc)
     short myAge = 13;
     std::cout << "my age is: " << myAge << std::endl;
-    // long - to archive long number
-    long lineOfCodes = 2323L; // needs L suffix after values
-    std::cout << "this pull request has " << lineOfCodes << " line of codes" << std::endl;
+    // long - to store long number
+    long linesOfCode = 2323L; // needs L suffix after values
+    std::cout << "this pull request has " << linesOfCode << " lines of code" << std::endl;
     // long long - to archive VERY LONG number (like my ORCID id)
     long long myORCID = 234567876987LL; // example only, lol, it needs LL suffix
     std::cout << "my ORCID: " << myORCID << std::endl;
