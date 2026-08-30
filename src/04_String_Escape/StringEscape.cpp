c0e85648d06f2fa74c992ecef8e04cbe690aae99
Dr. YFish <261328128+dryfish09@users.noreply.github.com>
2026-08-30 17:21:44 +0700

    Create StringEscape.cpp

---
+++ src/04_String_Escape/StringEscape.cpp
@@ -0,0 +1,21 @@
+/*
+filename: StringEscape.cpp
+license: unlicense
+*/
+#include <iostream>
+#include <string>
+int main() {
+    std::cout << "Hi there!\n"; // '\n' will creates new line (like std::endl, but it's faster)
+    // endl = '\n' + flush() so it's slower.
+    std::string greeting = "hello\n I'm don\n"; // also work with string
+    std::cout << greeting;
+    // tab escape:
+    std::cout << "hi\tthere" << std::endl; //output: hi   there
+    // backspace escape
+    std::cout << "hello\b" << std::endl; // output: hell
+    // unicode escape
+    std::string obfuscatedText = "\u0054\u0068\u0069\u0073\u0020\u006C\u0069\u006E\u0065\u0020\u0068\u0061\u0073\u0020\u0062\u0065\u0065\u006E\u0020\u006F\u0062\u0066\u0075\u0073\u0063\u0061\u0074\u0065\u0064";
+    std::cout << obfuscatedText << std::endl;
+    return 0;
+}
+// you can learn more [o]in internet
