2d7e7d6dc0f47f336abc5fa50ccf0464fa50e6c9
Dr. YFish <261328128+dryfish09@users.noreply.github.com>
2026-08-30 11:54:25 +0700

    feat: add if and else example

---
+++ src/03_Conditions/IfAndElse.cpp
@@ -0,0 +1,19 @@
+/*
+File name: IfAndElse.cpp
+License: The Unlicense
+*/
+#include <iostream>
+int main() {
+    int age = 13;
+    // if condition, to check if condition is correct as expected, do something
+    if (age == 13) {
+        std::cout << "you're 13 years old" << std::endl;
+    }
+    // else if - another if condition
+    else if (age > 13) {
+        std::cout<< "you're older than 13 years old" <<std::endl;
+    } else {
+        // else - if all check failed, do this
+        std::cout << "you younger than me" << std::endl;
+    }
+}
