f8525babd217d20aae3e043a7fba9bbac14602c7
Dr. YFish <261328128+dryfish09@users.noreply.github.com>
2026-08-31 06:52:46 +0700

    Add static cast

---
+++ src/02_Variables/05_TypeCasting.cpp
@@ -0,0 +1,16 @@
+/*
+file name: TypeCasting.cpp
+license: The Unlicense
+*/
+#include <iostream>
+int main() {
+    // int -> double
+    int ten = 10; // ten in english is 10
+    double infiniteFloat = static_cast<double>(maxPoint) / 3;
+    std::cout << ten << ": 3 = " << infiniteFloat << std::endl;
+    // double -> int
+    double pi = 3.14159;
+    int intergerPi = static_cast<int>(pi);
+    std::cout << "interger type of " << pi << "is " << intergerPi << std::endl;
+    return 0;
+}
