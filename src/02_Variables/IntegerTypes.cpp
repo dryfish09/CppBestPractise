f29a9489188cdbaad0617ef08965acbcd3bd0c2e
Dr. YFish <261328128+dryfish09@users.noreply.github.com>
2026-08-30 10:35:47 +0700

    Fix typos

--- src/02_Variables/IntegerTypes.cpp
+++ src/02_Variables/IntegerTypes.cpp
@@ -6,14 +6,14 @@ License: The Unlicense
 int main() {
     // int - store normal number, for normal use.
     int favouriteNumber = 36;
-    std::cout << "my favourite numberis: " << favouriteNumber << std::endl;
-    // short - only store small number (like age, classroom on your school, etc)
+    std::cout << "my favourite number is: " << favouriteNumber << std::endl;
+    // short - only store small number (like age, classroom in your school, etc)
     short myAge = 13;
     std::cout << "my age is: " << myAge << std::endl;
     // long - to store long number
     long linesOfCode = 2323L; // needs L suffix after values
     std::cout << "this pull request has " << linesOfCode << " lines of code" << std::endl;
-    // long long - to archive VERY LONG number (like my ORCID id)
+    // long long - to store VERY LONG number (like my ORCID id)
     long long myORCID = 234567876987LL; // example only, lol, it needs LL suffix
     std::cout << "my ORCID: " << myORCID << std::endl;
     // use 'unsigned' keyword if your values needs unsigned
