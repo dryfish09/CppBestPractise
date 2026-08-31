772f5df6853d7dc4db4cb41609ec5fb7c855ffdd
Dr. YFish <261328128+dryfish09@users.noreply.github.com>
2026-09-01 06:15:55 +0700

    feat: update logic example

--- src/05_Loops/02_While.cpp
+++ src/05_Loops/02_While.cpp
@@ -7,10 +7,13 @@ int main() {
     // while loop - loop until ...
     // you can add bool yourBool = true here
     while (true) { // you can replace 'true' with yourBool. Like: while (yourBool)
+        // code body here, write anything you want.
         int score = 999;
         if (score == 999) {
+            std::cout << "you have the biggest score, excellent!" << std::endl;
             break; // exit loop, if you don't have, program will never ends.
-        }
+        } else {
+            std::cout << "Score is smaller than 999" << std::endl;
     }
     return 0;
 }
