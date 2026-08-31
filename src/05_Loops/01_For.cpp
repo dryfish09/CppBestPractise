bc13c7def17bf7594482bb3b489b741f4c89c2b9
Dr. YFish <261328128+dryfish09@users.noreply.github.com>
2026-08-31 08:08:48 +0700

    feat: add condition example in for loop

--- src/05_Loops/01_For.cpp
+++ src/05_Loops/01_For.cpp
@@ -9,6 +9,12 @@ int main () {
         // loop body here
         std::cout << "looping " << i << " times" << std::endl; // repeat 4 times, because i starts at 0.
         // Loops 4 times: i = 0, 1, 2, 3
+        // you can add if - else here to check, like this:
+        if (i == 0) {
+            std::cout << "Started!" << std::endl;
+        } else {
+            continue;// do nothing, skip this loop time
+        }
     }
     return 0;
 }
