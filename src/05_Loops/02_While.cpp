383a5c2f7783fc1d9dae7429a0c5cdde0ca12bf1
Dr. YFish <261328128+dryfish09@users.noreply.github.com>
2026-08-31 19:35:10 +0700

    feat: add `while` loop code example

---
+++ src/05_Loops/02_While.cpp
@@ -0,0 +1,16 @@
+/*
+file name: While.cpp
+license: The Unlicense
+*/
+#include <iostream>
+int main() {
+    // while loop - loop until ...
+    // you can add bool yourBool = true here
+    while (true) { // you can replace 'true' with yourBool. Like: while (yourBool)
+        int score = 999;
+        if (score == 999) {
+            break; // exit loop, if you don't have, program will never ends.
+        }
+    }
+    return 0;
+}
