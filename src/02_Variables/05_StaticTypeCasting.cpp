76a46698fb403249d76272c12ec88dada8240239
Dr. YFish <261328128+dryfish09@users.noreply.github.com>
2026-09-01 12:24:52 +0700

    fix: fix syntax error

--- src/02_Variables/05_StaticTypeCasting.cpp
+++ src/02_Variables/05_StaticTypeCasting.cpp
@@ -7,7 +7,7 @@ NOTE: dynamic_cast will have with OOP.
 int main() {
     // int -> double
     int ten = 10; // ten in english is 10
-    double infiniteFloat = static_cast<double>(maxPoint) / 3;
+    double infiniteFloat = static_cast<double>(ten) / 3;
     std::cout << ten << ": 3 = " << infiniteFloat << std::endl;
     // double -> int
     double pi = 3.14159;
