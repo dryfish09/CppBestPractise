d16325ea41b811be41d1e608db1f906d58e8121c
Dr. YFish <261328128+dryfish09@users.noreply.github.com>
2026-09-01 18:42:01 +0700

    fix: correct code logic and remove trashes

--- src/07_Arrays/01_Arrays.cpp
+++ src/07_Arrays/01_Arrays.cpp
@@ -5,15 +5,13 @@ license: The Unlicense
 #include <iostream>
 #include <string>
 int main() {
-    // 3 ways for creating an integer array
-    // way 1: just declare
-    int scores[10];
-    // way 2: declare and fill with your custom values:
+    // 2 ways for creating an integer array
+    // way 1: declare and fill with your custom values:
     int smallNumber[9] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-    // way 3: declare, fill your custom values, C++ will caculate how many values in it
+    // way 2: declare, fill your custom values, C++ will caculate how many values in it
     int bigNumber[] = {100, 200, 400};
     // access to it:
-    std::cout << "my score is: " << scores[0] << std::endl; // array starts at 0, ends at max value declared - 1, like max value is 9, but it's 8.
+    // array starts at 0, ends at max value declared - 1, like max value is 9, but index 8.
     // you can access to array by variables:
     int myNumber = smallNumber[8]; // 9
     std::cout << "my number is " << myNumber << std::endl;
